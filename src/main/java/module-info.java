a5b4585a867151748d1a69926742e6dfa072393d
LOSS98 <134796259+LOSS98@users.noreply.github.com>
2025-01-06 10:10:06 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module fr.insa.horodateurjava {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens fr.insa.horodateurjava to javafx.fxml;
+    exports fr.insa.horodateurjava;
+}
