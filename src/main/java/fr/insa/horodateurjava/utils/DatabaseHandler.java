c114760e98a6beebc591f570942c440a24a1145b
LOSS98 <134796259+LOSS98@users.noreply.github.com>
2025-01-09 15:35:37 +0100

    🔐 Database link update

--- src/main/java/fr/insa/horodateurjava/utils/DatabaseHandler.java
+++ src/main/java/fr/insa/horodateurjava/utils/DatabaseHandler.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DatabaseHandler {
 
-    private static final String URL = "jdbc:sqlite:G:\\horodateur-java\\src\\main\\resources\\fr\\insa\\horodateurjava\\database.db";
+    private static final String URL = "jdbc:sqlite:src\\main\\resources\\fr\\insa\\horodateurjava\\database.db";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL);
