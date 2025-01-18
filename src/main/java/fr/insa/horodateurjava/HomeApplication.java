bfd810f33820cec5ed9d1e9a9b6245d389872f58
alixPyrd <alix.p2002@gmail.com>
2025-01-18 12:32:05 +0100

    Fichier lancement application

---
+++ src/main/java/fr/insa/horodateurjava/HomeApplication.java
@@ -0,0 +1,22 @@
+package fr.insa.horodateurjava;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class HomeApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(HomeApplication.class.getResource("views/home-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Accueil - Gestion des Places de Parking");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
